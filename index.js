ff6ab7f8c56e3478d158ff206b2e3886bdd35ea9
fokhrul2029 <ifokhrul.2029@gmail.com>
2024-05-06 16:08:33 +0600

    create a route to get a singleData from servicess

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -30,6 +30,16 @@ async function run() {
     app.get("/services", async (req, res) => {
       const cursor = services.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = {
+        projection: { title: 1, img: 1, price: 1 },
+      };
+      const result = await services.findOne(query, options);
       res.send(result);
     });
 
