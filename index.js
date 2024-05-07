c782653fc8c7d6712245fa5d1aae7e3c9c51429b
fokhrul2029 <ifokhrul.2029@gmail.com>
2024-05-08 00:07:23 +0600

    created a router to delete bookings data

--- index.js
+++ index.js
@@ -45,6 +45,23 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/bookings", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const cursor = bookings.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookings.deleteOne(query);
+      res.send(result);
+    });
+
     app.post("/bookings", async (req, res) => {
       const booking = req.body;
       const result = await bookings.insertOne(booking);
